ac134381cb1e12732c3f20063a2c959bcebc8389
Wesley Rodrigues Silva <161177795+Wesley-Rodrigues-Silva@users.noreply.github.com>
2024-04-24 18:37:25 -0300

    atualizações

---
+++ 44_set_timeout_e_sel_interval/js/script.js
@@ -0,0 +1,14 @@
+// setTimeout
+console.log('Antes do setTimeout')
+
+setTimeout(function() {
+    console.log('Testando o setTimeout...')
+
+}, 2000);
+
+console.log('Depois do setTimout...')
+
+// setInterval
+setInterval(function(){
+    console.log('Testando o setInterval...')
+}, 1000);
